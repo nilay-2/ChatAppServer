bc2d8397108de4d6414f2f735d1261ab55e5b50f
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-06-01 14:26:44 +0530

    added headers to socket connection

--- socket.js
+++ socket.js
@@ -12,16 +12,8 @@ exports.registerSocketServer = (server) => {
   const io = socket(server, {
     cors: {
       // origin: "http://localhost:3000",
-      origin: "https://chatvibe.vercel.app",
+      origin: "https://chatvibe.vercel.app/",
       methods: ["GET", "POST", "PATCH"],
-      allowRequest: (req, callback) => {
-        callback(null, {
-          cors: {
-            origin: req.headers.origin,
-            methods: ["GET", "POST", "PATCH"],
-          },
-        });
-      },
     },
   });
 
