21f60f156018f2f15f0b04728820a909672b1c2d
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-06-01 13:31:53 +0530

    added frontend url

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://chatvibe.vercel.app",
+    // origin: "http://localhost:3000",
     credentials: true,
   })
 );
