d5652674581dcd0bad57c39d4a2045fbbd5c694e
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-06-01 14:03:19 +0530

    added headers to socket connection

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chatvibe.vercel.app",
+    origin: "chatvibe.vercel.app",
     // origin: "http://localhost:3000",
     credentials: true,
   })
