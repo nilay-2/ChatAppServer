b1b7c832b7b5307c0da7d491c5db9a550b637dfb
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-06-07 21:09:29 +0530

    added logout request

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route("/signUp").post(authController.signUp);
 router.route("/login").post(authController.login);
 router.route("/updatePassword").post(authController.protect, authController.updatePassword);
-router.route("/logout").get(authController.logout);
+router.route("/logout").delete(authController.logout);
 // verify jwt token before making a socket connection in the frontend
 
 router.route("/verify").get(authController.protect, authController.allowUsersOnDashboard);
