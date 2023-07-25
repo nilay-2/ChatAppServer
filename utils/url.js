46e2bfdd21eeed637aab80486654a629c34824d4
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-07-25 18:15:37 +0530

    added cookie config

--- utils/url.js
+++ utils/url.js
@@ -2,4 +2,5 @@ exports.frontEnd_Prod_Url = "https://chatsphereclient.netlify.app";
 exports.frontEnd_Dev_Url = "http://localhost:3000";
 
 exports.backend_Dev_Domain = "localhost";
-exports.backend_Prod_Domain = "chatsphereserver.up.railway.app";
+// exports.backend_Prod_Domain = "chatsphereserver.up.railway.app";
+exports.backend_Prod_Domain = "chatserver-bs6i.onrender.com";
